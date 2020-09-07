c5fa4bf1e7702f72ed17b7d9bd41d3723024c552
cherrishccl <528050239@qq.com>
2020-09-07 18:13:35 +0800

    test

--- basics-tools/src/main/java/com/boot/basics/coding/tool/BatchTest.java
+++ basics-tools/src/main/java/com/boot/basics/coding/tool/BatchTest.java
@@ -13,7 +13,9 @@ import java.sql.SQLException;
  */
 public class BatchTest {
     public static void main(String[] args) {
-
+        batch("com.mysql.cj.jdbc.Driver",
+                "jdbc:mysql://localhost:3306/test?serverTimezone=Asia/Shanghai&characterEncoding=utf8&useUnicode=true&useSSL=false&allowMultiQueries=true&rewriteBatchedStatements=true",
+                "root", "123456");
     }
 
     private static void batch(String driver, String url, String username, String password){
@@ -30,15 +32,36 @@ public class BatchTest {
         }
         if(null != conn){
             PreparedStatement stmt = null;
+            /*try {
+                conn.setAutoCommit(false);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }*/
             try {
                 stmt =conn.prepareStatement("select * from dual");
             } catch (SQLException e) {
                 e.printStackTrace();
             }
             if(null != stmt){
-                stmt.addBatch();
+                for(int i = 0; i < 10000; i++){
+                    String batchSql = "insert into t_user(user_name, age, salary, max_size) values (" +
+                            "'name" + i + "'," + i + "," + i + "," + i +
+                            ")";
+                    try {
+                        stmt.addBatch(batchSql);
+                    } catch (SQLException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-            int[] rs = stmt.executeBatch();
+            try {
+                long start = System.currentTimeMillis();
+                int[] rs = stmt.executeBatch();
+                conn.commit();
+                System.out.println("total ======> " + (System.currentTimeMillis() - start));
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
         }
     }
 }

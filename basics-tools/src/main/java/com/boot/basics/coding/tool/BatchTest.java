7706849399ebaa1b436d096b073c687b34c97d8b
cherrishccl <528050239@qq.com>
2020-09-08 16:24:02 +0800

    批量

--- basics-tools/src/main/java/com/boot/basics/coding/tool/BatchTest.java
+++ basics-tools/src/main/java/com/boot/basics/coding/tool/BatchTest.java
@@ -9,7 +9,7 @@ import java.sql.SQLException;
  * @Author cherrishccl
  * @Date 2020/9/4 17:01
  * @Version 1.0
- * @Description
+ * @Description addBatch() 执行1次, addBatch(sql)执行N次
  */
 public class BatchTest {
     public static void main(String[] args) {
@@ -32,23 +32,32 @@ public class BatchTest {
         }
         if(null != conn){
             PreparedStatement stmt = null;
-            /*try {
+            try {
                 conn.setAutoCommit(false);
             } catch (SQLException e) {
                 e.printStackTrace();
-            }*/
+            }
+            String pSql = "insert into t_user(user_name, age, salary, max_size) values (?, ?, ?, ?)";
+            pSql = "update t_user set age = ?, salary = ?, max_size = ? where id = ?";
             try {
-                stmt =conn.prepareStatement("select * from dual");
+                // stmt =conn.prepareStatement("select * from dual");
+                stmt =conn.prepareStatement(pSql);
             } catch (SQLException e) {
                 e.printStackTrace();
             }
             if(null != stmt){
-                for(int i = 0; i < 10000; i++){
-                    String batchSql = "insert into t_user(user_name, age, salary, max_size) values (" +
+                for(int i = 1; i <= 10000; i++){
+                    /*String batchSql = "insert into t_user(user_name, age, salary, max_size) values (" +
                             "'name" + i + "'," + i + "," + i + "," + i +
-                            ")";
+                            ")";*/
                     try {
-                        stmt.addBatch(batchSql);
+                        // stmt.setString(1, "name" + i);
+                        stmt.setInt(1, i + 1);
+                        stmt.setInt(2, i + 1000);
+                        stmt.setInt(3, i + 1);
+                        stmt.setInt(4, i);
+                        //stmt.addBatch(batchSql);
+                        stmt.addBatch();
                     } catch (SQLException e) {
                         e.printStackTrace();
                     }

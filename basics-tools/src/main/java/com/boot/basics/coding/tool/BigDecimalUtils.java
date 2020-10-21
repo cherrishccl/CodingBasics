1cddc42b31487684a04558ee85bb73f16192c28a
cherrishccl <528050239@qq.com>
2020-10-21 16:42:07 +0800

    BigDecimalUtils

---
+++ basics-tools/src/main/java/com/boot/basics/coding/tool/BigDecimalUtils.java
@@ -0,0 +1,13 @@
+package com.boot.basics.coding.tool;
+
+import java.math.BigDecimal;
+
+/**
+ * @Author cherrishccl
+ * @Date 2020/10/21 16:31
+ * @Version 1.0
+ * @Description
+ */
+public class BigDecimalUtils {
+    private BigDecimalUtils(){}
+}

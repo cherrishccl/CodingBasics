be0b67f53abd202eeb38d5e50802303452334d29
cherrishccl <528050239@qq.com>
2020-12-08 14:16:19 +0800

    webflux

---
+++ basics-webflux/src/main/java/com/boot/basics/WebFluxApplication.java
@@ -0,0 +1,17 @@
+package com.boot.basics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author cherrishccl
+ * @Date 2020/12/8 13:47
+ * @Version 1.0
+ * @Description
+ */
+@SpringBootApplication
+public class WebFluxApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebFluxApplication.class, args);
+    }
+}
---
+++ basics-webflux/src/main/java/com/boot/basics/webflux/TestController.java
@@ -0,0 +1,51 @@
+package com.boot.basics.webflux;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.concurrent.TimeUnit;
+import java.util.stream.IntStream;
+
+/**
+ * @Author chencl
+ * @Date 2020/12/8 13:53
+ * @Version 1.0
+ * @Description
+ */
+@RestController
+@RequestMapping(value = "/test")
+public class TestController {
+
+    @GetMapping(value = "/mono")
+    public Mono<Object> mono() {
+        return Mono.create(monoSink -> {
+            System.out.println("创建 Mono");
+            monoSink.success("hello webflux");
+        }).doOnSubscribe(subscription -> {
+             System.out.println(subscription);
+        }).doOnNext(o -> {
+            System.out.println(o);
+        });
+    }
+
+    @GetMapping("flux")
+    public Flux<String> flux() {
+        return Flux.just("hello","webflux","spring","boot");
+    }
+
+    @GetMapping("/pub")
+    public Flux<String> pub(){
+        Flux<String> result = Flux
+                .fromStream(IntStream.range(1, 5).mapToObj(i -> {
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                    }
+                    return "flux data--" + i+"\n";
+                }));
+        return result;
+    }
+}

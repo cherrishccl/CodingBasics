50da6184ea71992e067fb0ef6e1060a9ba86d8dd
cherrishccl <528050239@qq.com>
2021-06-24 15:06:11 +0800

    jol

---
+++ basics-ctest/src/main/java/com/boot/basics/coding/JolTest.java
@@ -0,0 +1,32 @@
+package com.boot.basics.coding;
+
+import org.openjdk.jol.info.ClassLayout;
+
+import java.time.LocalDate;
+
+/**
+ * @Author chencl
+ * @Date 2021/6/24 14:54
+ * @Version 1.0
+ * @Description JolTest
+ */
+public class JolTest {
+    private Integer num;
+    private Long ll;
+    private long l;
+    private LocalDate[] ii;
+
+    public LocalDate[] getII(){
+        return ii;
+    }
+    public static void main(String[] args) {
+        Object o = new Object();
+
+        System.out.println(ClassLayout.parseInstance(o).toPrintable());
+
+        JolTest jolTest = new JolTest();
+
+        System.out.println(ClassLayout.parseInstance(jolTest).toPrintable());
+    }
+
+}

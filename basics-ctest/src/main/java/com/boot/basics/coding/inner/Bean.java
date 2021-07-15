409f9fb30663b976ac475f93aed5c70b68b0007d
cherrishccl <528050239@qq.com>
2021-07-15 15:52:54 +0800

    inner class

---
+++ basics-ctest/src/main/java/com/boot/basics/coding/inner/Bean.java
@@ -0,0 +1,16 @@
+package com.boot.basics.coding.inner;
+
+/**
+ * @Author cherrishccl
+ * @Date 2021/7/15 15:46
+ * @Version 1.0
+ * @Description Outer
+ */
+public class Bean {
+    class Bean1{}
+    static class Bean2{}
+}
+
+class Beann{
+    class Bean3{}
+}
---
+++ basics-ctest/src/main/java/com/boot/basics/coding/inner/Test.java
@@ -0,0 +1,20 @@
+package com.boot.basics.coding.inner;
+
+
+/**
+ * @Author cherrishccl
+ * @Date 2021/7/15 15:44
+ * @Version 1.0
+ * @Description Test
+ */
+public class Test {
+    public static void main(String[] args) {
+        Bean b = new Bean();
+        Bean.Bean1 b1 = b.new Bean1();
+        Bean.Bean2 b2 = new Bean.Bean2();
+        Beann beann = new Beann();
+        Beann.Bean3 b3 = beann.new Bean3();
+    }
+
+}
+
